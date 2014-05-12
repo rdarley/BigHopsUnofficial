f626baa18ee5f18213b4c73f5f606d357bb357b9
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2014-05-12 16:58:07 -0500

    Fix parsers!

--- app/parsers/nokoParse.rb
+++ app/parsers/nokoParse.rb
@@ -2,6 +2,48 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("http://bighops.com/taplist-huebner/"))
-table = /<table>.*<\/table>/.match(page)
-puts table
+class ParseBigHops
+
+  def initialize(url)
+    #New instance of beer array
+    @beers = Array.new
+    
+    page  = Nokogiri::HTML(open(url))
+    rows = page.css("tbody").css("tr").to_a
+    #Consider using xsl to grab this data itn eh future to prevent the need for code changes if the site is updated.
+    rows.each do |row|
+       data  = row.xpath("./td/text()").to_a
+       #for big hops we shoudl remove the asterisk and then trim whitespace for the brewery.  We can use untapped to get location
+       brewery = data[0].to_s
+       name = data[1].to_s
+       style = data[2].to_s
+       price = data[3].to_s
+       abv = data[4].to_s
+       @beers.push Beer.new(brewery, name, style, price, abv)
+    end
+  end #intialize
+
+  def list
+    @beers.each do |beer|
+      puts beer.to_s
+    end
+  end
+end
+
+class Beer
+  def  initialize (brewery, name, style, price, abv)
+    @brewery = brewery
+    @name= name
+    @style = style
+    @price = price
+    @abv = abv
+  end
+
+  def to_s
+    return @brewery + " " + @name + " "  + @style + " " + @price + " "  + @abv
+  end
+end
+
+thing = ParseBigHops.new("http://bighops.com/taplist-huebner/")
+thing.list
+
--- app/parsers/parse.rb
+++ app/parsers/parse.rb
@@ -1,28 +1,44 @@
 require 'curb'
 class ParseBigHops
 
-  def intitialze(url)
+  def initialize(url)
+    #New instance of beer array
+    @beers = Array.new
     c = Curl::Easy.perform(url)
     body = c.body_str
-    table = /<table.*<\/table>/.match(body)
-    rows = /<tr>.*<\/tr>/.match(table)
-    puts rows.class
+    tbody = body.scan(/<tbody>(.*?)<\/tbody>/)
+    rows = tbody.to_s.scan(/<tr>(.*?)<\/tr>/)
+    rows.each do |row|
+       data = row.to_a[0].scan(/<td>(.*?)<\/td>/)
+       brewery = data[0][0]
+       name = data[1][0]
+       style = data[2][0]
+       price = data[3][0]
+       abv = data[4][0]
+       @beers.push Beer.new(brewery, name, style, price, abv)
+    end
   end #intialize
 
-
-
-
+  def list
+    @beers.each do |beer|
+      puts beer.to_s
+    end
+  end
 end
 
 class Beer
-  def  initialize (brewery, name, style, price, abv, texan)
+  def  initialize (brewery, name, style, price, abv)
     @brewery = brewery
     @name= name
     @style = style
     @price = price
     @abv = abv
-    @texan = texan
+  end
+
+  def to_s
+    return @brewery + " " + @name + " "  + @style + " " + @price + " "  + @abv
   end
 end
 
-ParseBigHops("http://bighops.com/taplist-huebner/")
+thing = ParseBigHops.new("http://bighops.com/taplist-huebner/")
+thing.list
